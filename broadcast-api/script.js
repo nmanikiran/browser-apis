97c4ff64c764b52bb645d331a7e29e4dac204815
nmanikiran <nmanikiran15@gmail.com>
2020-07-25 00:21:25 +0530

    Added Broadcast Channel API

---
+++ broadcast-api/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const $title = document.querySelector('#title');
+  const $form = document.querySelector('form');
+  const $support = document.querySelector('#support');
+  $support.hidden = true;
+  let bc;
+  try {
+    bc = new BroadcastChannel('test_channel');
+  } catch (error) {
+    $support.hidden = false;
+  }
+
+  const postMessage = () => {
+    const title = $title.value;
+    if (!title) return;
+    bc.postMessage(title);
+    setTimeout(() => {
+      $title.value = '';
+    }, 1000);
+  };
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postMessage();
+  });
+
+  bc.onmessage = function (ev) {
+    document.title = ev.data;
+  };
+});
